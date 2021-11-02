8579b784731d5ee3672f10a1499768b01456b5df
csagastegui59 <csagastegui59@gmail.com>
2021-11-01 21:03:55 -0500

    Products and categories tests

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-<<<<<<< HEAD
-import { PrismaService } from 'src/prisma/prisma.service';
-import { AttachmentService } from 'src/attachment/attachment.service';
-=======
-import { PrismaService } from '../prisma-service/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AttachmentService } from '../attachment/attachment.service';
->>>>>>> categories_test
 
 @Module({
   controllers: [ProductsController],
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,19 +1,11 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-<<<<<<< HEAD
-import { PrismaService } from 'src/prisma/prisma.service';
-import { PaginationQueryDto } from 'src/pagination/dtos/pagination-query.dto';
-import { paginatedHelper } from 'src/pagination/pagination.helper';
-import { paginationSerializer } from 'src/pagination/serializer';
-import { AttachmentService } from 'src/attachment/attachment.service';
-=======
-import { PrismaService } from '../prisma-service/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { PaginationQueryDto } from '../pagination/dtos/pagination-query.dto';
 import { paginatedHelper } from '../pagination/pagination.helper';
 import { paginationSerializer } from '../pagination/serializer';
 import { AttachmentService } from '../attachment/attachment.service';
->>>>>>> categories_test
 import { plainToClass } from 'class-transformer';
 import { ProductDto } from './dto/product.dto';
 import { DetailedProductDto } from './dto/detailed-product.dto';
@@ -115,10 +107,7 @@ export class ProductsService {
   ): Promise<{ data: DetailedProductDto }> {
     if (!isManager) {
       throw new HttpException(
-        errorMessage(
-          HttpStatus.FORBIDDEN,
-          'ONLY MANAGERS CAN VIEW DISABLED PRODUCTS',
-        ),
+        errorMessage(HttpStatus.FORBIDDEN, 'ONLY MANAGERS CAN UPDATE PRODUCTS'),
         HttpStatus.FORBIDDEN,
       );
     }
@@ -152,10 +141,7 @@ export class ProductsService {
   async remove(id: number, isManager: boolean): Promise<void> {
     if (!isManager) {
       throw new HttpException(
-        errorMessage(
-          HttpStatus.FORBIDDEN,
-          'ONLY MANAGERS CAN VIEW DISABLED PRODUCTS',
-        ),
+        errorMessage(HttpStatus.FORBIDDEN, 'ONLY MANAGERS CAN DELETE PRODUCTS'),
         HttpStatus.FORBIDDEN,
       );
     }
@@ -175,7 +161,7 @@ export class ProductsService {
       throw new HttpException(
         errorMessage(
           HttpStatus.FORBIDDEN,
-          'ONLY MANAGERS CAN VIEW DISABLED PRODUCTS',
+          'ONLY MANAGERS CAN CHANGE PRODUCTS AVAILABILITY',
         ),
         HttpStatus.FORBIDDEN,
       );
