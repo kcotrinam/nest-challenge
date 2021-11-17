2863448cbc917a83ba8a204d3fcae705bdbc5c89
csagastegui59 <csagastegui59@gmail.com>
2021-11-17 16:12:52 -0500

    Add pagination to categories

--- src/categories/resolvers/categories.resolver.ts
+++ src/categories/resolvers/categories.resolver.ts
@@ -1,5 +1,5 @@
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { CategoryModel } from '../models/category.model';
+import { CategoryModel, PaginatedCategories } from '../models/category.model';
 import { CategoriesService } from '../categories.service';
 import { CreateCategoryModel } from '../models/create-category.model';
 import { UpdateCategoryModel } from '../models/upcate-category.model';
@@ -8,22 +8,23 @@ import { RolesGuard } from '../../auth/guards/role.guard';
 import { CurrentUser } from '../../auth/decorators/curret-user.decorator';
 import { GqlAuthGuard } from '../../auth/guards/gql-auth.guard';
 import { CurrentUserGuard } from '../../auth/guards/curretn-user.guard';
+import { PaginationGql } from 'src/utils/args/pagination.args';
 
 @Resolver(() => CategoryModel)
 export class CategoriesResolver {
   constructor(private readonly categoriesService: CategoriesService) {}
 
-  @Query(() => [CategoryModel])
-  async categories(
-    @Args('page', { type: () => Int }) page: number,
-    @Args('perPage', { type: () => Int }) perPage: number,
-  ) {
-    const categories = await this.categoriesService.findAll({
-      page,
-      perPage,
-    });
+  @Query(() => PaginatedCategories)
+  async categories(@Args() pagination: PaginationGql) {
+    const categories = await this.categoriesService.findAll(
+      {
+        page: pagination.page,
+        perPage: pagination.perPage,
+      },
+      false,
+    );
 
-    return categories.data;
+    return categories;
   }
 
   @Query(() => CategoryModel)
